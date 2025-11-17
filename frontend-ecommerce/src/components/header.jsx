b6559c579eb4a1279f93623dc23600e7a5c1825e
NataliaTEC <Natalia01rg@estudiantec.cr>
2025-11-17 00:56:26 -0600

    cambios en el haeder

--- frontend-ecommerce/src/components/header.jsx
+++ frontend-ecommerce/src/components/header.jsx
@@ -5,6 +5,7 @@ import searchIcon from '../assets/icons/microfono.svg'
 import cuentaIcon from '../assets/icons/user.svg'
 import cartIcon from '../assets/icons/bag.svg'
 import LangIcon from '../assets/icons/language.svg'
+import WishIcon from '../assets/icons/heart.svg'
 import './header.css'
 import categories from '../data/categories'
 import storesIcon from '../assets/icons/stores.svg'
@@ -127,9 +128,7 @@ export default function Header() {
 
           <Link to="/wishlist" className="wishlist-button" aria-label="Wishlist">
             <span className="icon-circle">
-              <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                <path d="M20.8 4.6a5.5 5.5 0 0 0-7.8 0L12 5.6l-1-1a5.5 5.5 0 0 0-7.8 7.8L12 21l8.8-8.6a5.5 5.5 0 0 0 0-7.8z"></path>
-              </svg>
+              <img src={WishIcon} alt="Wishlist" className="svg-icon wish-icon" />
             </span>
             <span className="icon-badge">0</span>
           </Link>
